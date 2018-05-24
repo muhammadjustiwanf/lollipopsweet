d21bd8fb3f58b7b575e5c2e110c159c475b24621
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-24 17:24:03 +0700

    Add files via upload

--- handler/robotkerang.php
+++ handler/robotkerang.php
@@ -3,13 +3,13 @@
 require_once('./line_class.php');
 $channelAccessToken = 'ISI_DISINI'; //Channel access token
 $channelSecret = 'ISI_DISINI';//Channel secret
-
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-$replyToken = $client->parseEvents()[0]['replyToken'];
-$message 	= $client->parseEvents()[0]['message'];
-$msg_type = $message['type'];
-$botname = "KerangAjaib"; //Nama bot
 */
+	$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
+	$replyToken = $client->parseEvents()[0]['replyToken'];
+	$message 	= $client->parseEvents()[0]['message'];
+	$msg_type = $message['type'];
+	$botname = "robotkerangdb";
+
 function send($input, $rt){
     $send = array(
         'replyToken' => $rt,
@@ -23,25 +23,26 @@ function send($input, $rt){
     return($send);
 }
 
-function jawabs(){
-    $list_jwb = array(
+function answers(){
+    $answers_list = array(
 		'Ya',
 		'Tidak',
 		'Bisa jadi',
 		'Mungkin',
 		'Tentu tidak',
-		'Coba tanya lagi'
+		'Coba tanya lagi',
+		'Saya Tidak tahu'
 		);
-    $jaws = array_rand($list_jwb);
-    $jawab = $list_jwb[$jaws];
-    return($jawab);
+    $answr = array_rand($answers_list);
+    $answrr = $list_jwb[$answr];
+    return($answrr);
 }
 
 if($msg_type == 'text'){
     $pesan_datang = strtolower($message['text']);
     $filter = explode(' ', $pesan_datang);
-    if($filter[0] == 'apakah') {
-        $balas = send(jawabs(), $replyToken);
+    if($filter[0] == '.apakah', '.bagaimana', '.kapan') {
+        $balas = send(answers(), $replyToken);
     } else {}
 } else {}
 
