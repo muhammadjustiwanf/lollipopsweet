867f93923e8c43758eba8da518570e08a3762483
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-24 19:21:46 +0700

    Add files via upload

---
+++ handler/instagram.php
@@ -0,0 +1,29 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder as ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function instagram($userName){
+
+				$URL = 'https://www.instagram.com/' . $userName;
+				$instagramHtmlCode = file_get_contents($URL);
+				$regex = '/"display_url":"(.*?)"/';
+
+				preg_match_all($regex, $instagramHtmlCode, $result);
+
+				$urlFound = $result[1];
+				$urlCount = count($urlFound);
+
+				$randomIndex = rand(0, $urlCount - 1);
+				$randomUrl = $urlFound[$randomIndex];
+
+				if ($randomUrl == null){
+						$result = new TextMessageBuilder('Foto pada akun tidak ditemukan.');
+				} else {
+						$result = new ImageMessageBuilder($randomUrl, $randomUrl);
+				}
+		}
+
+		return $result;
+
+}
