ed6f276ad44aca4f96d1e2df04b030efdfa58543
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-24 19:12:42 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 use LINE\LINEBot\SignatureValidator as SignatureValidator;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 foreach (glob("handler/*.php") as $handler){
-		if ($handler != 'handler/post.php', 'handler/robotkerang.php'){
+		if ($handler != 'handler/post.php', $handler != 'handler/robotkerang.php'){
 				include $handler;
 		}
 }
