838389a34ec50e8222329ab4e26dd454f4dea90e
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-25 10:45:56 +0700

    Add files via upload

--- handler/mathjs.php
+++ handler/mathjs.php
@@ -6,6 +6,9 @@ function calculate($query) {
 
 		$query = urlencode($query);
 		$result = file_get_contents('http://api.mathjs.org/v4/?expr=' . $query);
+				if ($query == null){
+        $result = new TextMessageBuilder("Untuk cara menggunakan kalkulator, ketik /calculate 'nominal'\n\nContoh: /calculate 5+5*5-5\n\nSelamat mencoba (((o(*ﾟ▽ﾟ*)o)))");
+		} else {
 		$result = new TextMessageBuilder($result);
 
 		return $result;
