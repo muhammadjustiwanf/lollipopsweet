e04e154fea93fb0be967fbb460bedf682f55338f
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-25 11:12:01 +0700

    Add files via upload

--- handler/mathjs.php
+++ handler/mathjs.php
@@ -9,13 +9,13 @@ function calculate($query){
 				} else {
 
 				$query = urlencode($query);
-				$result = file_get_contents('http://api.mathjs.org/v4/?expr=' . $query);
+				$result_num = file_get_contents('http://api.mathjs.org/v4/?expr=' . $query);
 
 				if ($result == null){
 						$result = new TextMessageBuilder('Nominal tidak terdefinisi');
 						} else {
 
-						$result = new TextMessageBuilder($result);
+						$result_num = new TextMessageBuilder($result);
 						}
 
 						return $result;
