af1c37181cb944f79ee1180d18e6477583ce4c10
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-25 12:18:34 +0700

    Add files via upload

--- handler/robotkerang.php
+++ handler/robotkerang.php
@@ -2,53 +2,14 @@
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-/*
---------------------------------------------------------------- NOTICE ME...
-				
-				$inputMessage = $event['message']['text'];
-				$userId = $event['source']['userId'];
-
-				if ($inputMessage[0] == '/') {
-
-					 $inputMessage = ltrim($inputMessage, '/');
-					 $inputSplit = explode(' ', $inputMessage, 2);
-
-					 if ( function_exists( $inputSplit[0] ) ){
-
-							$outputMessage = $inputSplit[0]( $inputSplit[1], $userId );
-
-					 } else {
-				$outputMessage = new TextMessageBuilder("tipe command tidak ditemukan, coba kakak ketik:\n\n.help\n\nbiar kakak tau apa aja commandnya :v");
-					 }
-				
-				$result = $bot->replyMessage($event['replyToken'], $outputMessage);
-				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
-} else {
-
-				$wordsLearned = file_get_contents('https://bot-line-multifunction.firebaseio.com/words.json');
-				$wordsLearned = json_decode($wordsLearned, true);
-
-				foreach ($wordsLearned as $word => $answer) {
-						if (strpos(strtolower($inputMessage), $word) !== false) {
-								$outputMessage = new TextMessageBuilder($answer);
-								$result = $bot->replyMessage($event['replyToken'], $outputMessage);
-								return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-								break;
-						}
-				}
-
-}
-				
-				// --------------------------------------------------------------- ...SENPAI!
-*/
-
 $botname = "robotkerangdb";
 
-function send($input, $rt){
+function send($input){
 if ($input == null){
 $result = new TextMessageBuilder("Puja kulit kerang ajaib ululululululu..... :v\n\nCara menggunakannya:\nKetik: /apakah kata2 yang ingin diajukan.\n\nContoh: /apakah bot pintar?\n\nSelamat mencoba :v");
 } else {
+$result = new TextMessageBuilder($input);
+/*
     $send = array(
         'replyToken' => $rt,
         'messages' => array(
@@ -58,7 +19,8 @@ $result = new TextMessageBuilder("Puja kulit kerang ajaib ululululululu..... :v\
             )
         )
     );
-    return($send);
+*/
+    return $result;
 }
 
 function aswers(){
@@ -78,7 +40,8 @@ function aswers(){
 if(strtolower($inputMessage){
     $input = explode(' ', $inputMessage);
     if($input[0] == 'apakah'){
-        $result = new TextMessageBuilder(send(answers(), rand($answerslist));
+        $balas = send(answers());
+        $result = new TextMessageBuilder(json_encode, $balas);
     }
 }
 return $result;
