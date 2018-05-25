cf64a5663fc52cd4be7f8d4d96d15e32465cf451
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-25 14:41:05 +0700

    Add files via upload

---
+++ id.php
@@ -0,0 +1,15 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function userid($query, $userId) {
+
+		if ($userId == null) {
+				$result = new TextMessageBuilder('Hai Kak ' . $profile->displayName . ' Kita belum berteman, add dulu gih :v');
+		} else {
+				$result = new TextMessageBuilder('Hai Kak ' . $profile->displayName . ' Ini adalah userId kakak: ' . $userId);
+		}
+
+		return $result;
+
+}
