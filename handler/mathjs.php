42d5d13e1409d37f68806c406568acbc9fa74431
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-25 10:56:10 +0700

    Add files via upload

--- handler/mathjs.php
+++ handler/mathjs.php
@@ -4,16 +4,17 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function calculate($query) {
 
-		if ($query == null){
-     $result = new TextMessageBuilder("Untuk cara menggunakan kalkulator, ketik /calculate 'nominal'\n\nContoh: /calculate 5+5*5-5\n\nSelamat mencoba (((o(*ﾟ▽ﾟ*)o)))");
-		} else {
 		$query = urlencode($query);
 		$result = file_get_contents('http://api.mathjs.org/v4/?expr=' . $query);
-		$result = new TextMessageBuilder($result);
 
-		return $result;
+				if ($query == null){
+				$result = new TextMessageBuilder("Untuk cara menggunakan kalkulator, ketik /calculate 'nominal'\n\nContoh: /calculate 5+5*5-5\n\nSelamat mencoba (((o(*ﾟ▽ﾟ*)o)))");
+				} else {
+				$result = new TextMessageBuilder($result);
+
+				return $result;
+
+				}
 
 		}
-
-}
 
