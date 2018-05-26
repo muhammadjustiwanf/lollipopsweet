cb5b3432d16b43cac90a68637fd62f4f7a2f440d
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 20:33:59 +0700

    Add files via upload

--- robot/samehadaku.php
+++ robot/samehadaku.php
@@ -9,25 +9,25 @@ include 'postsamehada.php';
 				$result = new TextMessageBuilder('use /samehadaku to view list update anime');
 } else {
 
-$samehadaku = samehadaku("https://www.samehadaku.tv");
-$dom = new DomDocument();
-$dom->loadHTML($samehadaku);
-$classname="post-item tie-standard";
-$finder = new DomXPath($dom);
-$spaner = $finder->query("//*[contains(@class, '$classname')]");
-$span = $spaner->item(0);
-$link =  $span->getElementsByTagName('a');
-$tanggal = $span->getElementsByTagName('span');
-$no = 0;
-$data =array();
-foreach ($link as $val){ 
-    $data[] = array(
-        'judul' => $link->item($no)->nodeValue,
-        'link' => $link->item($no)->getAttribute('href'),
-        'tanggal' => $tanggal->item($no)->nodeValue,
-        );
+$samehadaku = samehadaku("https://www.samehadaku.tv");
+$dom = new DomDocument();
+$dom->loadHTML($samehadaku);
+$classname="post-item tie-standard";
+$finder = new DomXPath($dom);
+$spaner = $finder->query("//*[contains(@class, '$classname')]");
+$span = $spaner->item(0);
+$link =  $span->getElementsByTagName('a');
+$tanggal = $span->getElementsByTagName('span');
+$no = 0;
+$data =array();
+foreach ($link as $val){
+    $data[] = array(
+        'judul' => $link->item($no)->nodeValue,
+        'link' => $link->item($no)->getAttribute('href'),
+        'tanggal' => $tanggal->item($no)->nodeValue,
+        );
 }
-$result = new TextMessageBuilder($data[]);
+$result = new TextMessageBuilder($data[]);
 }
 return $result;
     $no++;
