f2b4e6f052b81ab0705a6b3dfb871bcbf781a61b
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 09:45:54 +0700

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+require_once('./LINEBotTiny.php');
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+foreach (glob("bot/*.php") as $bot){include $bot;}
+
+//var_dump($client->parseEvents());
+
+
+//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
+
+// load config
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+// initiate app
+$configs =  [
+	'settings' => ['displayErrorDetails' => true],
+];
+$app = new Slim\App($configs);
+
+/* ROUTES */
+$app->get('/', function ($request, $response) {
+	return "Lanjutkan!";
+});
+
+$app->post('/', function ($request, $response)
+{
+	// get request body and line signature header
+	$body 	   = file_get_contents('php://input');
+	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+	// log body and signature
+	file_put_contents('php://stderr', 'Body: '.$body);
+
+	// is LINE_SIGNATURE exists in request header?
+	if (empty($signature)){
+		return $response->withStatus(400, 'Signature not set');
+	}
+
+	// is this request comes from LINE?
+	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+		return $response->withStatus(400, 'Invalid signature');
+	}
+
+	// init bot
+	$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+	$data = json_decode($body, true);
+	foreach ($data['events'] as $event)
+		if ($event['type'] == 'message')
+
+	$userId 	= $client->parseEvents()[0]['source']['userId'];
+	$replyToken = $client->parseEvents()[0]['replyToken'];
+	$timestamp	= $client->parseEvents()[0]['timestamp'];
+	$type 		= $client->parseEvents()[0]['type'];
+	$message 	= $client->parseEvents()[0]['message'];
+	$messageid 	= $client->parseEvents()[0]['message']['id'];
+	$profil = $client->profil($userId);
