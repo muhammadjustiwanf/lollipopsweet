06bdfb50dd7f6abc95d10aeef7f69fd95f6255ff
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 16:12:28 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -4,12 +4,17 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 $url = 'https://www.samehadaku.net/';
 
-function samehadaku($query){
+function samehadaku($query, $data){
+
+if ($data == null){
+$result = new TextMessageBuilder ('Not Found');
+} else {
 
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $result = curl_exec($ch); 
-    curl_close($ch);    
+    curl_close($ch);  
+  }
     return $result;
 }
