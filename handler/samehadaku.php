ca3775ea0bb46be95e7f7a3e532ef858b7947656
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 16:06:55 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -10,7 +10,6 @@ function samehadaku($query){
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
     $result = curl_exec($ch); 
-    curl_close($ch);
-}      
+    curl_close($ch);    
     return $result;
 }
