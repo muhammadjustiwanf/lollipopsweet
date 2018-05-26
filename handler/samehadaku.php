db5ce23a2604ea64691d3d70158b4f1fd7323d97
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 16:16:23 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -13,7 +13,7 @@ $result = new TextMessageBuilder ('Not Found');
     $ch = curl_init(); 
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-    $result = curl_exec($ch); 
+    $result = new TextMessageBuilder(curl_exec($ch)); 
     curl_close($ch);  
   }
     return $result;
