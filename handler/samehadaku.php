4ff47985c3685ee475405c1574ad4f61d3c2068d
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 16:54:05 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function samehadaku($query){
 
-$session = curl_init('"http://jurnalcode.com/");
+$session = curl_init("http://jurnalcode.com/");
 $result = curl_exec($session);
 curl_close($session);
 return $result;
