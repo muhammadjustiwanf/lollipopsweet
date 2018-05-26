6846938faa576639abf4891115fa505af7cdf175
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 17:31:57 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -12,19 +12,17 @@ return $result;
 */
 
 function cuaca($url){
-if ($url == null){
-$result = new TextMessageBuilder('Not Found');
-} else {
+
      $data = curl_init();
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
      curl_setopt($data, CURLOPT_URL, $url);
      curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
-     $result = new TextMessageBuilder (curl_exec($data));
+     $result = curl_exec($data);
      curl_close($data);
      return $result;
 }
-$url = cuaca('http://www.bmkg.go.id/BMKG_Pusat/Informasi_Cuaca/Prakiraan_Cuaca/Prakiraan_Cuaca_Indonesia.bmkg');
-$pecah = explode('<h1>Prakiraan Cuaca Indonesia</h1>',$url);
+
+$url = cuaca('https://www.samehadaku.tv/');
+$pecah = explode('ul',{'class':'post-item tie standard'},$url);
 $result = new TextMessageBuilder($pecah2[0]);
 return $result;
-}
