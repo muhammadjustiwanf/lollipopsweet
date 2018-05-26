3464ff14a3b988dd8c9d9fc9775306f9b74ea79e
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 12:01:50 +0700

    Add files via upload

--- handler/id.php
+++ handler/id.php
@@ -3,12 +3,12 @@
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 use \LINE\LINEBot\Response;
 
-function userid($query, $userId, $profile){
+function userid($profile, $userId){
 
 		if ($userId == null){
 				$result = new TextMessageBuilder('Kita belum berteman kak, add dulu gih :v');
 				} else {
-				$result = new TextMessageBuilder('Hai kak ' . $displayName . ' Ini adalah userid kakak: ' . $userId);
+				$result = new TextMessageBuilder('Hai kak ' . $profile->displayName . ' Ini adalah userid kakak: ' . $userId);
 		}
 
 		return $result;
