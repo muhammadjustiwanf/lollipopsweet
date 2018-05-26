e37a7febbc31945e3b5a3ef5678433a69647c47d
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 17:17:27 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -12,15 +12,19 @@ return $result;
 */
 
 function cuaca($url){
+if ($url == null){
+$result = new TextMessageBuilder('Not Found');
+} else {
      $data = curl_init();
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
      curl_setopt($data, CURLOPT_URL, $url);
      curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
-     $hasil = curl_exec($data);
+     $result = new TextMessageBuilder (curl_exec($data));
      curl_close($data);
-     return $hasil;
+     return $result;
 }
-$url = fungsiCurl('http://www.bmkg.go.id/BMKG_Pusat/Informasi_Cuaca/Prakiraan_Cuaca/Prakiraan_Cuaca_Indonesia.bmkg');
+$url = cuaca('http://www.bmkg.go.id/BMKG_Pusat/Informasi_Cuaca/Prakiraan_Cuaca/Prakiraan_Cuaca_Indonesia.bmkg');
 $pecah = explode('<h1>Prakiraan Cuaca Indonesia</h1>',$url);
 $result = new TextMessageBuilder($pecah2[0]);
 return $result;
+}
