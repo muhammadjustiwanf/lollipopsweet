3ce67b905b3be32d4b8fe8b6a1db52a5e05135e6
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 18:06:40 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -4,15 +4,23 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function samehadaku($url){
 
-     $data = curl_init();
-     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-     curl_setopt($data, CURLOPT_URL, $url);
-     $result = curl_exec($data);
-     curl_close($data);
-     return $result;
+  if ($url == null){
+      $result == new TextMessageBuilder ('Not Found');
+} else {
+      $data = curl_init();
+      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($data, CURLOPT_URL, $url);
+      $result = curl_exec($data);
+      curl_close($data);
+}
+      return $result;
 }
 
 $url = samehadaku('https://www.samehadaku.tv/');
 $pecah = explode('<ul class="post-item tie-standard"',$url);
+  if ($pecah == null){
+      $result = new TextMessageBuilder('Hasil tidak ditemukan');
+} else {
 $result = new TextMessageBuilder($pecah[0]);
+}
 return $result;
