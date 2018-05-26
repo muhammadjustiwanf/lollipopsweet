055350dcc99fe1147480c15fe3299cf197358802
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 13:05:36 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require 'vendor/autoload.php';
 
+use \LINE\LINEBot;
 use LINE\LINEBot\SignatureValidator as SignatureValidator;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 foreach (glob("handler/*.php") as $handler){
