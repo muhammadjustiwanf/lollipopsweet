d6606cdc71b5994225e051969b723404f9df3f43
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 11:29:17 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $app->post('/', function ($request, $response)
 	
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	//$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
-	$bot = new \LINE\LINEBot(new CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']), [
+	$bot = new \LINE\LINEBot(new CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN'], [
     'channelSecret' => $_ENV['CHANNEL_SECRET']
 ]);
 
