60dd8cc2f0f4d8d12279ba216d99eb92aff3fac2
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-26 13:43:13 +0700

    Add files via upload

--- handler/id.php
+++ handler/id.php
@@ -7,7 +7,7 @@ function userid($query, $userId){
 		if ($userId == null){
 				$result = new TextMessageBuilder("Kita belum berteman kak, ".$profile['displayName']." add dulu gih :v");
 				} else {
-				$result = new TextMessageBuilder("Hai kak ".$profile['displayName']." Ini adalah userid kakak:\n".$userId);
+				$result = new TextMessageBuilder("Hai kak ".$profile['displayName']." Ini adalah userid kakak:\n\nUserId:".$userId."\n\nDisimpan baik2 yah UserId nya :)");
 		}
 
 		return $result;
