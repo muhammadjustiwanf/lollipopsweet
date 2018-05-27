522ce61d277623b4463fae5a25eaa31a50b57f84
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-27 10:23:57 +0700

    Add files via upload

--- handler/confirmtemplate.php
+++ handler/confirmtemplate.php
@@ -1,20 +1,23 @@
 <?php
 
 use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder as ConfirmTemplateBuilder;
-use \LINE\LINEBot\MesssgeBuilder\TextMessageBuilder as TextMessageBuilder;
+use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder as MessageTemplateActionBuilder;
+use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder as TemplateMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function postmember($inputMessage){
 if ($inputMessage == null){
     $result = new TextMessageBuilder('Mau memposting orang? Caranya: /postmember orang.');
 } else {
-    $result = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder(
+    $result = new ConfirmTemplateBuilder(
    "apakah gw ganteng?",
    [
-   new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Ya',"/ya"),
-   new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Tidak','/tidak'),
+   new MessageTemplateActionBuilder('Ya',"/ya"),
+   new MessageTemplateActionBuilder('Tidak','/tidak'),
    ]
    );
-$templateMessage = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('nama template', $result);
+$templateMessage = new TemplateMessageBuilder('nama template', $result);
 $result = $bot->replyMessage($event['replyToken'], $templateMessage);
-return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-}}
+}
+return $result;
+}
