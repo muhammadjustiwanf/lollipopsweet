fac7503ab27d11a71fbebeee088b3738c7e169a3
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-29 04:43:53 +0700

    Add files via upload

--- handler/robotkerang.php
+++ handler/robotkerang.php
@@ -4,21 +4,16 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 $botname = "robotkerangdb";
 
-function apakah($inputMessage, $rt){
-  if ($inputMessage == null){
+function apakah($query, $userId){
+  if ($query == null){
     $result = new TextMessageBuilder("Puja kulit kerang ajaib ululululululu......\nCara menggunakannya:\n\n.apakah [pertanyaanmu].\nMisal: .apakah bot pintar?\n\nSilahkan dicoba");
   } else {
 
-    $send = array(
-        'replyToken' => $rt,
-        'messages' => array(
-            array(
-                'type' => 'text',					
-                'text' => $inputMessage
-            )
-        )
-    );
+   $result = new TextMessageBuilder($query);
+   return $result;
+} else {
 
+    $send = array($query);
     $answerslist = array(
 		'Ya',
 		'Iya',
@@ -29,7 +24,7 @@ function apakah($inputMessage, $rt){
 		);
     $answr = array_rand($answerslist);
     $answrr = $answerslist[$answr];
-    $reply = $send[$answrr];
+    $reply = $result[$answrr];
 
     $result = new TextMessageBuilder(json_encode($reply));
   }
