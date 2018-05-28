4c89783c938067500ecec31f216bf79542741465
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-28 10:04:45 +0700

    Add files via upload

--- handler/post.php
+++ handler/post.php
@@ -1,15 +1,15 @@
 <?php
 
 function postData($path, $data){
+	
+	$URL = 'https://bot-line-multifunction.firebaseio.com/' . $path . '.json';
 
-		$URL = 'https://bot-line-multifunction.firebaseio.com/' . $path . '.json';
-
-		$curl = curl_init();
-		curl_setopt( $curl, CURLOPT_URL, $URL );
-		curl_setopt( $curl, CURLOPT_CUSTOMERQUEST, "PUT" );
-		curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode($data));
-		curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-		$response = curl_exec( $curl );
-		curl_close( $curl );
-
+	$curl = curl_init();
+	curl_setopt( $curl, CURLOPT_URL, $URL );
+	curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "PUT" );
+	curl_setopt( $curl, CURLOPT_POSTFIELDS, json_encode($data));
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+	$response = curl_exec( $curl );
+	curl_close( $curl );
+	
 }
--- handler/teach.php
+++ handler/teach.php
@@ -4,30 +4,31 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function teach($query, $userId){
 
-		include 'post.php';
-		if ($userId != 'U45a70016f56dbfc99e6a66673002ecbe'){
-				$result = new TextMessageBuilder('Akses Ditolak.');
+	include 'post.php';
+	if ($userId != 'U45a70016f56dbfc99e6a66673002ecbe'){
+		$result = new TextMessageBuilder('Akses Ditolak.');
+	} else {
+
+		if ($query == null){
+			$result = new TextMessageBuilder("Ajari bot kata-kata atau keyword\n\n/teach [keyword] [jawaban] \n\njika tidak terdapat jawaban, maka secara otomatis [keyword] akan terhapus.");
 		} else {
 
-				if ($query == null){
-						$result = new TextMessageBuilder("Ajari bot kata-kata atau keyword\n\n/teach [keyword] [jawaban] \n\njika tidak terdapat jawaban, maka secara otomatis [keyword] akan terhapus.");
-				} else {
+			$querySplit = explode(' ', $query, 2);
+			$word = strtolower($querySplit[0]);
+			$answer = $querySplit[1];
 
-						$querySplit = explode(' ', $query, 2);
-						$word = strtolower($querySplit[0]);
-						$answer = $querySplit[1];
+			postData('words/' . $word, $answer);
 
-						postData('words/' . $word, $answer);
+			if ($answer == null){
+				$result = new TextMessageBuilder('Jawaban untuk "' . $word . '" telah dihapus.');
+			} else {
+				$result = new TextMessageBuilder('Terimakasih sudah mengajari bot kata2, silahkan coba ketik "' . $word . '".');
+			}
 
-						if ($answer == null){
-								$result = new TextMessageBuilder('Jawaban untuk "' . $word . '" telah dihapus.');
-						} else {
-								$result = new TextMessageBuilder('Terimakasih sudah mengajari bot kata2, silahkan coba ketik "' . $word . '".');
-						}
-				}
 		}
+	}
 
-		return $result;
+	return $result;
 
 }
 
