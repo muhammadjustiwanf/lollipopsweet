d68ce336bb2e3725cc20ea7fbd2a82942489a462
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-29 12:00:08 +0700

    Add files via upload

--- handler/samehadaku.php
+++ handler/samehadaku.php
@@ -1,32 +1,25 @@
 <?php
 
+require('simple_html_dom.php');
+
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function youtube($userId){
 
-// Create DOM from URL or file
 $html = file_get_html('https://www.youtube.com/feed/trending');
-
-// creating an array of elements
 $videos = [];
-
-// Find top ten videos
 $i = 1;
 foreach ($html->find('li.expanded-shelf-content-item-wrapper') as $video) {
         if ($i > 10) {
                 break;
         }
 
-        // Find item link element
         $videoDetails = $video->find('a.yt-uix-tile-link', 0);
 
-        // get title attribute
         $videoTitle = $videoDetails->title;
 
-        // get href attribute
         $videoUrl = 'https://youtube.com' . $videoDetails->href;
 
-        // push to a list of videos
         $videos[] = [
                 'title' => $videoTitle,
                 'url' => $videoUrl
