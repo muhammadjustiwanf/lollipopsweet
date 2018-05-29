c0ae72dbcbda754f5789c128f5aab897d9eef8ef
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-29 17:29:30 +0700

    Add files via upload

--- handler/simsimi.php
+++ handler/simsimi.php
@@ -5,12 +5,12 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 function simsimi($query, $userId){
   if ($query == null){
     $result = new TextMessageBuilder('Simsimi ada disini!\nCoba ketik .simsimi [keyword] untuk memulai percakapan.\n\nContoh: .simsimi apa kabar?\n\nSilahkan dicoba~');
-  } else {
+  } else
 
       if($message['type']=='sticker'){	
         $result = new TextMessageBuilder('Terimakasih stikernya :)');
 
-      } else
+      } else {
         $pesan=str_replace(" ", "%20", $query);
         $key = '26d4b177-b900-4f68-8099-0d2c02252d1a'
         $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
@@ -19,14 +19,15 @@ function simsimi($query, $userId){
         $diterima = $url['response'];
         if($url['result'] == 404){
           $result = new TextMessageBuilder('Harap gunakan bahasa yang dapat dimengerti, jangan pakek bahasa alien :v');
-        } else {
+        } else
           if($url['result'] != 100){
           $result = new TextMessageBuilder('Maaf '.$profil['displayName'].' Server Kami Sedang Sibuk Sekarang.
           } else {
             $result = new TextMessageBuilder($diterima);
-	          }
+	          } else {
             $result = new TextMessageBuilder(json_encode($result));
             file_put_contents('./reply.json',$result);
+         }
   }
             return $result;
 }
