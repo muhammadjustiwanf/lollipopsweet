a948c130b721e0247ffb382edfd40f8ca019b2d2
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-31 06:24:56 +0700

    Add files via upload

--- handler/coryn.php
+++ handler/coryn.php
@@ -10,20 +10,21 @@ function corynlv($query, $userId){
 		$result = new TextMessageBuilder(".corynlv [level]\n\nSilahkan dicoba  (((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
+    if ($i == 8){
 		$query = urlencode($query);
 		$URL = $URL . '?lv=' . $query;
 		
 		$json = file_get_contents($URL);
 		$json = json_decode($json, true);
 		
-		$hasil = $json['result'][0]['hasil'];
+		$hasil = $json['result'][i]['hasil'];
 		
-		if ($hasil == null){
-			$result = new TextMessageBuilder('GeneratorID tidak ditemukan.');
-		} else {
-			$result = new TextMessageBuilder('GeneratorID: ' . $hasil);
-		}
-		
+		  if ($hasil == null){
+		 	  $result = new TextMessageBuilder('GeneratorID tidak ditemukan.');
+		  } else {
+			  $result = new TextMessageBuilder('GeneratorID: ' . $hasil);
+		  }
+		}
 	}
 	
 	return $result;
