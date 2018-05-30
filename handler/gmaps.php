fd126a6313f6c0d724d086d5643cb137db57e422
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-30 10:59:46 +0700

    Add files via upload

--- handler/gmaps.php
+++ handler/gmaps.php
@@ -1,53 +1,49 @@
-<?php 
+<?php
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function gmaps($query, $userId){
+	
+	$URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+	$apiKey = 'AIzaSyDUC3p9FqzmMYgYWKiBpLcQqMddot48xyw';
+	
+	if ($query == null){
+		$result = new TextMessageBuilder("Meme Generator\n\nCara menggunakan:\n.memeid query\n.meme 'memeid' Teks atas|Teks bawah\n\nContoh:\n.memeid Kocak\n/meme 689854 Kids zaman now|Generasi Micin.\n\nSilahkan dicoba  (((o(*ﾟ▽ﾟ*)o)))");
+	} else {
+
+		$query = urlencode($query);
+		$URL = $URL . '?address=' . $query;
+		$URL = $URL . '&apiKey=' . $apiKey;
+		
+		$json = file_get_contents($URL);
+		$json = json_decode($json, true);
+
+    if($json['status']=='OK'){
  
-function geocode($address, $userId){
-  if ($address == null){
-    $result = new TextMessageBuilder('Jangan typo!');
-  } else {
- 
-    // url encode the address
-    $address = urlencode($address);
-     
-    // google map geocode api url
-    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyDUC3p9FqzmMYgYWKiBpLcQqMddot48xyw";
- 
-    // get the json response
-    $json = file_get_contents($url);
-     
-    // decode the json
-    $jsondecode = json_decode($json, true);
- 
-    // response status will be 'OK', if able to geocode given address 
-    if($resp['status']=='OK'){
- 
-        // get the important data
-        $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
-        $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
-        $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
-         
-        // verify if data is complete
+      $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+      $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+      $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+
         if($lati && $longi && $formatted_address){
          
-            // put the data in the array
-            $data_arr = array();            
+          $data_arr = array();            
              
-            array_push(
-                $data_arr, 
-                    $lati, 
-                    $longi, 
-                    $formatted_address
-                );
+          array_push(
+              $data_arr, 
+                  $lati, 
+                  $longi, 
+                  $formatted_address
+              );
+             
+	  	if ($data_arr == null){
+	  		$result = new TextMessageBuilder('false');
+		  } else {
+			  $result = new TextMessageBuilder($data_arr);
         }
-           if ($data_arr == null){
-             $result = new TextMessageBuilder('Not Found data!');
-           } else {
-            $result = new TextMessageBuilder($data_arr);
-             
-    }
-         
-  }
-            return $result;
-}
+	  }
+	}
+ }
+	
+	  return $result;
+
 }
