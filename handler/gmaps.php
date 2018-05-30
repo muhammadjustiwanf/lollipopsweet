f4dbae4f2cd668c388a45c423e7dd253b959284e
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-30 11:08:54 +0700

    Add files via upload

--- handler/gmaps.php
+++ handler/gmaps.php
@@ -8,7 +8,7 @@ function gmaps($query, $userId){
 	$apiKey = 'AIzaSyDUC3p9FqzmMYgYWKiBpLcQqMddot48xyw';
 	
 	if ($query == null){
-		$result = new TextMessageBuilder("Meme Generator\n\nCara menggunakan:\n.memeid query\n.meme 'memeid' Teks atas|Teks bawah\n\nContoh:\n.memeid Kocak\n/meme 689854 Kids zaman now|Generasi Micin.\n\nSilahkan dicoba  (((o(*ﾟ▽ﾟ*)o)))");
+		$result = new TextMessageBuilder("Google Maps Api.\n\nCara menggunakan: .gmaps [lokasi]\nMisal: .gmaps bandar lampung, kedaton.\n\nSilahkan dicoba (((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
 		$query = urlencode($query);
@@ -18,31 +18,31 @@ function gmaps($query, $userId){
 		$json = file_get_contents($URL);
 		$json = json_decode($json, true);
 
-    if($json['status']=='OK'){
+    if ($json['status'] == 'OK'){
  
-      $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
-      $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
-      $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+      $lati = isset(json['results'][0]['geometry']['location']['lat']) ? $json['results'][0]['geometry']['location']['lat'] : "";
+      $longi = isset($json['results'][0]['geometry']['location']['lng']) ? $json['results'][0]['geometry']['location']['lng'] : "";
+      $formatted_address = isset($json['results'][0]['formatted_address']) ? $json['results'][0]['formatted_address'] : "";
 
-        if($lati && $longi && $formatted_address){
+      if ($lati && $longi && $formatted_address){
          
-          $data_arr = array();            
+        $data_arr = array();            
              
-          array_push(
-              $data_arr, 
-                  $lati, 
-                  $longi, 
-                  $formatted_address
-              );
+        array_push(
+            $data_arr, 
+                $lati, 
+                $longi, 
+                $formatted_address
+        );
              
-	  	if ($data_arr == null){
+	  	  if ($data_arr == null){
 	  		$result = new TextMessageBuilder('false');
 		  } else {
 			  $result = new TextMessageBuilder($data_arr);
         }
+	    }
 	  }
-	}
- }
+  }
 	
 	  return $result;
 
