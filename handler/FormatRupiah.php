7aa47a3c9d0176614223ba3f4f72b00c431f7c14
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-31 17:37:13 +0700

    Add files via upload

---
+++ handler/FormatRupiah.php
@@ -0,0 +1,17 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+$angka = '1234567890';
+function rp($angka) {
+ 
+    if(is_numeric($angka)) {
+        $format_rupiah = 'Rp ' . number_format($angka, '2', ',', '.');
+        return $format_rupiah;
+    }
+    else {
+        $result = new TextMessageBuilder($angka . " bukan angka yang valid!" . "\n");
+    }
+$result = new TextMessageBuilder(formatRupiah($angka));
+return $result;
+}
