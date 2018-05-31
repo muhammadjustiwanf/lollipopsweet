128b6520f08749c4b11e465326f2f591df9ec249
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-31 17:03:04 +0700

    Add files via upload

--- handler/helpmenu.php
+++ handler/helpmenu.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function help($query, $userId){
 	
-	$result = new TextMessageBuilder("~List command yang tersedia~\nUSE [.] for prefix.\n\n.userid\n.instagram\n.calculate\n.meme\n.xkcd\n.teach\n\nSilahkan dicoba (*^▽^*)");
+	$result = new TextMessageBuilder("~List command yang tersedia~\nUSE [.] for prefix.\n\n→ .userid\n→ .instagram\n→ .calculate\n→ .meme\n→ .xkcd\n→ .teach\n→ .apakah\n→ .corynitem\n\nSilahkan dicoba (*^▽^*)");
 	return $result;
 	
 }
