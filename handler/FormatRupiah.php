9f046c4ef317f7b771d4e9873d979afbb3439c54
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-05-31 18:32:37 +0700

    Add files via upload

--- handler/FormatRupiah.php
+++ handler/FormatRupiah.php
@@ -5,16 +5,19 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 function rp($query, $userId) {
 
   if ($query == null){
-    $result = new TextMessageBuilder($query . " bukan angka yang valid!" . "\n");
+    $result = new TextMessageBuilder("Format Rupiah Generator.\n#EdisiMiminLagiGabut😆\n\nCara menggunakan: .rp [nominal]\nContoh: .rp 1000000 (Tidak memakai titik ataupun koma pada nominal, jika dipakai maka generator akan menghasilkan nilai yang salah).\n\nSilahkan dicoba~");
   } else
  
     if (is_numeric($query)) {
-      $format_rupiah = 'Rp ' . number_format($query, '2', ',', '.');
-      if ($format_rupiah == null){
-        $result = new TextMessageBuilder('Format rupiah gagal dikonversi');
-      } else {
-      $result = new TextMessageBuilder($format_rupiah);
-      }
+      $format_rupiah = 'Rp. ' . number_format($query, '2', ',', '.');
     }
-    return $result;
+      if ($format_rupiah == null){
+        $result = new TextMessageBuilder("Gagal mengidentifikasi nominal atau ". $query . " bukan nominal yang valid!" . "\n");
+      } else {
+        $result = new TextMessageBuilder($format_rupiah);
+        }
+
+  return $result;
+
 }
+
