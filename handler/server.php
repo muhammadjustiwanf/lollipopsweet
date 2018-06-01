9afc3f392d896c7c38b4427a6dd2201e42faf704
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 12:24:22 +0700

    Add files via upload

--- handler/server.php
+++ handler/server.php
@@ -4,13 +4,20 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function server($query, $userId){
 
-  $host = $query;
-  $port = 80;
-  if (!$socket = @fsockopen($host, $port, $errno, $errstr, 30)){
-    $result = new TextMessageBuilder($host . ' Sedang Down');
-  } else {
-    $result = new TextMessageBuilder($host . ' Online');
-  fclose($socket);
-    }
-  return $result;
+	if ($query == null){
+		$result = new TextMessageBuilder("~Server Checker~\n#EdisiGabut😂\n\nCommand ini dibuat dalam kondisi mimin yang lagi gabut 😆. Command ini berfungsi untuk mengecek suatu server website apakah sedang down/off atau online.\nCara menggunakan: .server [web]\nContoh: .server www.youtube.com\n\nSilahkan dicoba~ 😄");
+	} else {
+
+		$host = $query;
+		$port = 80;
+			if (!$socket = @fsockopen($host, $port, $errno, $errstr, 30)){
+				$result = new TextMessageBuilder($host . ' Sedang Down');
+			} else {
+				$result = new TextMessageBuilder($host . ' Online');
+				fclose($socket);
+				}
+		}
+
+	return $result;
+
 }
