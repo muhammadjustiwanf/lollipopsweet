adc5a661ab7cafb2bce6c66bd9b16653e05c96c6
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 15:14:47 +0700

    Add files via upload

--- handler/tanggal.php
+++ handler/tanggal.php
@@ -5,7 +5,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 function tanggal($userId){
 	
 	$date = date_default_timezone_set('Asia/Jakarta');
-	$result = new TextMessageBuilder($date("→Tanggal: " . j F Y . "\n→Jam: " . H:i:s));
+	$result = new TextMessageBuilder($date("→Tanggal = j F Y\n→Jam = H:i:s"));
 	return $result;
 	
 }
