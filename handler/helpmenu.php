ba53cb64a7d0c3abb4d61ac2b3f23956400addc2
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 12:45:48 +0700

    Add files via upload

--- handler/helpmenu.php
+++ handler/helpmenu.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function help($userId){
 	
-	$result = new TextMessageBuilder("~List command yang tersedia~\nUSE [.] for prefix.\n\n→ .userid\n→ .instagram\n→ .calculate\n→ .meme\n→ .xkcd\n→ .teach\n→ .apakah (Kerang ajaib)\n→ .corynitem\n→ .rp\n→ .tanggal\n→ .server\n\nSilahkan dicoba (*^▽^*)");
+	$result = new TextMessageBuilder("~List command yang tersedia~\n>USE [.] for prefix.<\nUntuk bantuan bagaimana cara menggunakannya, cukup ketik salah satu command yang ingin kamu pilih dibawah ini lalu *send* maka bot akan mengirimkan pesan bantuannya ^^\n\n→ .userid\n→ .instagram\n→ .calculate\n→ .meme\n→ .xkcd\n→ .teach\n→ .apakah (Kerang ajaib)\n→ .corynitem\n→ .rp\n→ .tanggal\n→ .server\n\nSilahkan dicoba (*^▽^*)");
 	return $result;
 	
 }
