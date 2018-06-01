72d7644bed11ca1d7a3ab416a40d28af2d1b28ea
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 10:05:07 +0700

    Add files via upload

--- handler/tanggal.php
+++ handler/tanggal.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function tanggal($userId){
 	
-	$result = new TextMessageBuilder(date('j-F-Y'));
+	$result = new TextMessageBuilder(date('j F Y'));
 	return $result;
 	
 }
