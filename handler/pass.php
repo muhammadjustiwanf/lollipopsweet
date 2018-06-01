15d0e4ee96e45416119ddf69d71a92034b0652bd
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 10:21:14 +0700

    Add files via upload

---
+++ handler/pass.php
@@ -0,0 +1,24 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function pass($query, $userId){
+
+  if ($query == null){
+    $result = new TextMessageBuilder('~Password Generator~\n\nCara menggunakan: .pass [input password]\nContoh: .pass golden\n\nSilahkan dicoba~');
+  } else {
+
+    for ($p = 0; $p < 10; $p++) {
+    $string .= $query[mt_rand(0, $ps)];
+    }
+    if ($string == null){
+      $result = new TextMessageBuilder('Error atau ' . $query . ' tidak valid!');
+    } else {
+      $result = new TextMessageBuilder($string);
+      }
+    }
+
+  return $result;
+
+}
+
