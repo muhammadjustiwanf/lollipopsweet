a209d35a0491955ab2a1ed93aeafdc578f337a4d
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 11:31:19 +0700

    Add files via upload

---
+++ handler/kemiripan.php
@@ -0,0 +1,21 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function berapapersen($query, $userId){
+
+  if ($query == null){
+    $result = new TextMessageBuilder("><");
+  } else {
+
+    similar_text($query, $persen);
+    if ($persen == null){
+      $result = new TextMessageBuilder('Null');
+    } else {
+      $result = new TextMessageBuilder('Kemiripan: ' . $persen . ' %');
+      }
+    }
+
+  return $result;
+
+}
