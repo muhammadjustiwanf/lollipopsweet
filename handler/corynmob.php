433d271a1f19bd3610d9ff4344db22d67d668373
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 19:09:15 +0700

    Add files via upload

---
+++ handler/corynmob.php
@@ -0,0 +1,26 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function corynmob($query, $userId){
+	
+	$URL = 'http://coryn.club/monster.php';
+	
+	if ($query == null){
+		$result = new TextMessageBuilder("Coryn Monster Search Engine.\n\nKetik: .corynmob [nama monster]\nContoh: .corynmob ifrid\n\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
+	} else {
+
+		$query = urlencode($query);
+		$URL = $URL . '?name=' . $query;
+		
+		if ($URL == null){
+			$result = new TextMessageBuilder('Monster ' . $query . ' tidak ditemukan.');
+		} else {
+			$result = new TextMessageBuilder('Klik: ' . $URL);
+		}
+		
+	}
+	
+	return $result;
+
+}
