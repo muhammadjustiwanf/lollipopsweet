8017557c36fdce3e53a8dc7db4a2cec5cab3f9f2
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 12:13:39 +0700

    Add files via upload

---
+++ handler/server.php
@@ -0,0 +1,16 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function server($query, $userId){
+
+  $host = $query;
+  $port = 80;
+  if (!$socket = @fsockopen($host, $port, $errno, $errstr, 30)){
+    $result = new TextMessageBuilder($host . ' Sedang Down');
+  } else {
+    $result = new TextMessageBuilder($host . ' Online');
+  fclose($socket);
+    }
+  return $result;
+}
