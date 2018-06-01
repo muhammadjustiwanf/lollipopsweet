a5d46c8a30c40909724e679c9c6b1d30e6daaa19
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 11:12:10 +0700

    Add files via upload

--- handler/robotkerang.php
+++ handler/robotkerang.php
@@ -5,26 +5,29 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 $botname = "robotkerangdb";
 
 function apakah($query, $userId){
-  if ($query == null){
-    $result = new TextMessageBuilder("Puja kulit kerang ajaib ululululululu......\nCara menggunakannya:\n\n.apakah [pertanyaanmu].\nMisal: .apakah bot pintar?\n\nSilahkan dicoba");
-  } else {
 
-    $answerslist = array(
+	if ($query == null){
+		$result = new TextMessageBuilder("Puja kulit kerang ajaib ululululululu......\nCara menggunakannya:\n\n.apakah [pertanyaanmu].\nMisal: .apakah bot pintar?\n\nSilahkan dicoba");
+	} else {
+
+		$answerslist = array(
 		'Iya',
 		'Kalo diliat2 lagi sih iya',
 		'Bisa jadi',
 		'Mungkin',
 		'Tidak',
 		'Coba tanya lagi',
-    'Benar',
-    'hoax'
+		'Benar',
+		'hoax'
 		);
-    $answr = array_rand($answerslist);
-    $answrr = $answerslist[$answr];
+		$answr = array_rand($answerslist);
+		$answrr = $answerslist[$answr];
 
-    $result = new TextMessageBuilder($answrr);
-  }
-  return $result;
-  file_put_contents($botname.'.json',$result);
+		$result = new TextMessageBuilder($answrr);
+	}
+
+	return $result;
+	file_put_contents($botname.'.json',$result);
+
 }
 
--- handler/robotkerang2.php
+++ handler/robotkerang2.php
@@ -2,29 +2,28 @@
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-$botname = "robotkerangdb";
-
 function dimana($query, $userId){
-  if ($query == null){
-    $result = new TextMessageBuilder("Ini bukan kerang ajaib, kalo kerang ajaib yang satunya :v\nCara menggunakannya:\n\n.dimana [pertanyaanmu].\nMisal: .dimana kamu tadi melihat dia?\n\nSilahkan dicoba");
-  } else {
 
-    $answerslist = array(
-		'di sayidan',
-		'dinamo',
-		'di tato',
-		'di tytyd',
-		'di puskud',
-		'dimana aja',
-    'di lampu merah',
-    'di goa'
+	if ($query == null){
+		$result = new TextMessageBuilder("Ini bukan kerang ajaib, kalo kerang ajaib yang satunya :v\nCara menggunakannya:\n\n.dimana [pertanyaanmu].\nMisal: .dimana kamu tadi melihat dia?\n\nSilahkan dicoba");
+	} else {
+
+		$answerslist = array(
+			'di sayidan',
+			'dinamo',
+			'di tato',
+			'di tytyd',
+			'di puskud',
+			'dimana aja',
+			'di lampu merah',
+			'di goa'
 		);
-    $answr = array_rand($answerslist);
-    $answrr = $answerslist[$answr];
+		$answr = array_rand($answerslist);
+		$answrr = $answerslist[$answr];
+		$result = new TextMessageBuilder($answrr);
+	}
 
-    $result = new TextMessageBuilder($answrr);
-  }
-  return $result;
-  file_put_contents($botname.'.json',$result);
+	return $result;
+
 }
 
