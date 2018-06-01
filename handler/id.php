5299f7f5a45064c89fa7541ef246c1f7e7b51ec9
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-01 11:13:55 +0700

    Add files via upload

--- handler/id.php
+++ handler/id.php
@@ -2,7 +2,7 @@
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-function userid($userId){
+function userid($query, $userId){
 	
 	if ($userId == null){
 		$result = new TextMessageBuilder('Kita belum berteman, add dulu gih sono lalu ketik .userid dan kirim di kolom chat :v');
