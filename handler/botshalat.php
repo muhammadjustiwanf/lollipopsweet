c7742a8602cb283dcbc722de3e7879a8a66ced97
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-04 20:20:11 +0700

    Add files via upload

---
+++ handler/botshalat.php
@@ -0,0 +1,47 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function jadwalshalat($query, $userId){
+	
+	//include 'line_class.php';
+	//include 'unirest-php-master/src/Unirest.php';
+	$URL = 'https://time.siswadi.com/';
+	
+	if ($query == null){
+		$result = new TextMessageBuilder("(((o(*ﾟ▽ﾟ*)o)))");
+	} else {
+
+		$query = urlencode($query);
+		$URL = $URL . 'pray/' . $query;
+		$response = Unirest\Request::get("$URL");
+		$json = json_decode($response->raw_body, true);
+		$hasil = array(
+		$teks = 'Jadwal Shalat Sekitar ' . $query . ':';
+		$teks .= $json['location']['address'];
+		$teks .= "\nTanggal : ";
+		$teks .= $json['time']['date'];
+		$teks .= "\n\nShubuh : ";
+		$teks .= $json['data']['Fajr'];
+		$teks .= "\nDzuhur : ";
+		$teks .= $json['data']['Dhuhr'];
+		$teks .= "\nAshar : ";
+		$teks .= $json['data']['Asr'];
+		$teks .= "\nMaghrib : ";
+		$teks .= $json['data']['Maghrib'];
+		$teks .= "\nIsya : ";
+		$teks .= $json['data']['Isha'];
+		return $hasil;
+);
+		
+		if ($hasil == null){
+			$result = new TextMessageBuilder('nothing');
+		} else {
+			$result = new TextMessageBuilder($hasil);
+		}
+		
+	}
+	
+	return $result;
+
+}
