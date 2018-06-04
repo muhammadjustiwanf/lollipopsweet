1c2d01f778b409b87e425eaf254c318f373a2728
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-04 17:14:22 +0700

    Add files via upload

---
+++ handler/convert.php
@@ -0,0 +1,25 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder as ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function write($query, $userId){
+
+  require_once 'TeksKeGambar.php';
+  if ($query == null){
+    $result = new TextMessageBuilder('TULIS SESUATU!');
+  } else {
+
+    $img = new TeksKeGambar;
+    $text = $query;
+    $img->buatGambar($text);
+
+    $image = $img->tampilkanGambar();
+    $result = new ImageMessageBuilder($image);
+    }
+
+  return $result;
+
+}
+
+//$img->simpanKePng('hasil-convert','gambar/');
