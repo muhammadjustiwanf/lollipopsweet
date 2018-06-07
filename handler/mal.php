9609b488f64c5e79c1cd516a803a6ea7958e74f9
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-07 22:50:58 +0700

    Add files via upload

--- handler/mal.php
+++ handler/mal.php
@@ -2,20 +2,20 @@
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-function malsearch($query, $userId){
+function malanime($query, $userId){
 	
 	include 'jikan-me/jikan';
 	date_default_timezone_set('Asia/Jakarta');
 	$jikan = new Jikan\Jikan;
-	$URL = 'https://myanimelist.net/character.php';
+	$URL = 'https://myanimelist.net/anime.php';
 	
 	if ($query == null){
-		$result = new TextMessageBuilder("Hmm... Cuaca hari ini mendung, cerah, atau hujan yah? Nih, bot bisa memberi tahu kamu prediksi cuaca di kota kamu lho..😆 Caranya cukup mudah, yuk cek dengan cara:\n\nKetik: .prediksicuaca [kota]\nContoh: .prediksicuaca Jakarta\n\nKarena ini prediksi, jadi tidak 100% akurat yah ^^\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
+		$result = new TextMessageBuilder("(((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
 		$query = urlencode($query);
 		$URL = $URL . '?q=' . $query;
-		$jikan->Search($URL, ANIME);
+		$jikan->Search($query, ANIME);
 		
 		if ($URL == null){
 			$result = new TextMessageBuilder('Error atau hasil pencarian tidak ditemukan. Silahkan coba lagi~');
