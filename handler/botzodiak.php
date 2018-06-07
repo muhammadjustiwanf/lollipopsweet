3a346661a4c5469ac4932c447551b3491efe9c90
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-07 19:11:41 +0700

    Add files via upload

--- handler/botzodiak.php
+++ handler/botzodiak.php
@@ -10,11 +10,11 @@ function zodiak($query, $displayName){
 	
 	$URL = 'https://script.google.com/macros/exec';
 	$appkey = 'AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w';
-	$nama = 'anda';
+	$nama = 'Anda';
 	$tanggal = $query;
 	
 	if ($query == null){
-		$result = new TextMessageBuilder("(((o(*ﾟ▽ﾟ*)o)))");
+		$result = new TextMessageBuilder("Ketik: .zodiak [tgl-bln-thn kamu]\nContoh: .zodiak 01-01-2000\n\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
 		$URL = $URL . '?service=' . $appkey;
@@ -24,10 +24,10 @@ function zodiak($query, $displayName){
 		$response = Unirest\Request::get("$URL");
 		$json = json_decode($response->raw_body, true);
 		
-		if (isset($json['success']))
-			$result = new TextMessageBuilder("Zodiak " . $nama . ":\n\nTanggal: " . $query . "\nLahir: " . $json['data']['lahir'] . "\nUsia: " . $json['data']['usia'] . "\nUltah: " . $json['data']['ultah'] . "\nZodiak: " . $json['data']['zodiak']);
-		else
+		if (isset($json['error']))
 			$result = new TextMessageBuilder('Tanggal ' . $query . ' tidak ditemukan. ngetik yang bener, jangan typo! :v');
+		else
+			$result = new TextMessageBuilder("Zodiak " . $nama . ":\n\nTanggal: " . $query . "\nLahir: " . $json['data']['lahir'] . "\nUsia: " . $json['data']['usia'] . "\nUltah: " . $json['data']['ultah'] . "\nZodiak: " . $json['data']['zodiak']);
 		
 		}
 	
