15f326359df54da63a13968afbbe49c76fe4bea8
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-07 17:45:35 +0700

    Add files via upload

--- handler/botzodiak.php
+++ handler/botzodiak.php
@@ -8,20 +8,24 @@ function zodiak($query, $userId){
 	include 'unirest-php-master/src/Unirest.php';
 	date_default_timezone_set('Asia/Jakarta');
 	
+	$res = $bot->getProfile('user-id');
+if ($res->isSucceeded()) {
+    $profile = $res->getJSONDecodedBody();
+    $displayName = $profile['displayName'];
+    $statusMessage = $profile['statusMessage'];
+    $pictureUrl = $profile['pictureUrl'];
+}
 	$URL = 'https://script.google.com/macros/exec';
 	$appkey = 'AKfycbw7gKzP-WYV2F5mc9RaR7yE3Ve1yN91Tjs91hp_jHSE02dSv9w';
+	$nama = $displayName;
 	$tanggal = $query;
 	
 	if ($query == null){
 		$result = new TextMessageBuilder("Hmm... Cuaca hari ini mendung, cerah, atau hujan yah? Nih, bot bisa memberi tahu kamu prediksi cuaca di kota kamu lho..😆 Caranya cukup mudah, yuk cek dengan cara:\n\nKetik: .prediksicuaca [kota]\nContoh: .prediksicuaca Jakarta\n\nKarena ini prediksi, jadi tidak 100% akurat yah ^^\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
-		$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
-		$userId = $client->parseEvents()[0]['source']['userId'];
-		$profil = $client->profil($userId);
-		
 		$URL = $URL . '?service=' . $appkey;
-		$URL = $URL . '&nama=' . $profil->displayName;
+		$URL = $URL . '&nama=' . $nama;
 		$URL = $URL . '&tanggal=' . $query;
 
 		$response = Unirest\Request::get("$URL");
