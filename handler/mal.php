3522e046909a63bf2c8b0385a97988fa1f9400bb
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-07 22:32:29 +0700

    Add files via upload

---
+++ handler/mal.php
@@ -0,0 +1,30 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function malsearch($query, $userId){
+	
+	include 'jikan-me/jikan';
+	date_default_timezone_set('Asia/Jakarta');
+	$URL = 'https://myanimelist.net/character.php';
+	
+	if ($query == null){
+		$result = new TextMessageBuilder("Hmm... Cuaca hari ini mendung, cerah, atau hujan yah? Nih, bot bisa memberi tahu kamu prediksi cuaca di kota kamu lho..😆 Caranya cukup mudah, yuk cek dengan cara:\n\nKetik: .prediksicuaca [kota]\nContoh: .prediksicuaca Jakarta\n\nKarena ini prediksi, jadi tidak 100% akurat yah ^^\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
+	} else {
+
+		$query = urlencode($query);
+		$URL = $URL . '?q=' . $query;
+		$jikan->Search($URL, ANIME);
+		
+		if ($URL == null){
+			$result = new TextMessageBuilder('Error atau hasil pencarian tidak ditemukan. Silahkan coba lagi~');
+		} else {
+			$result = new TextMessageBuilder(		$jikan->response['result']);
+			}
+		
+		}
+	
+	return $result;
+
+}
+
