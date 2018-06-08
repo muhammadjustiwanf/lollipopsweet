38c8fa22ba62932026f8ba1d63e18ec7f71e627b
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-08 09:41:09 +0700

    Add files via upload

--- handler/playstoresearch.php
+++ handler/playstoresearch.php
@@ -1,10 +1,11 @@
 <?php
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+use \source\core\playStoreApi as PlayStoreApi
 
 function playstore($query, $userId){
 	
-	include 'source/core/playStoreApi.php';
+	//include 'source/core/playStoreApi.php';
 	$class_init = new PlayStoreApi;
 	if ($query == null){
 		$result = new TextMessageBuilder("~Playstore App Search Engine~\n\nCara menggunakan: .playstore [app]\nContoh: .playstore whatsapp\n\nSilahkan dicoba~ :v");
@@ -23,12 +24,15 @@ function playstore($query, $userId){
 	// You can easily add the page numbers to paginate the result
 	//$page = 2;
 	//$searchStore = $class_init->searchStore($search_query,$sort,$price,$safe_search,$page);
-
-		if($searchStore !== 0)
-		{
-			print_r($searchStore);
-			$result = new TextMessageBuilder("Result of " . $query . ":\nSort by: " . $sort . "\nPrice: " . $price . "\nSafe Search: " . $safe_search . "\n\nResult:\n\n" . $searchStore);
-		}
+		
+		if ($searchStore == null){
+			$result = new TextMessageBuilder('Pencarian tidak ditemukan!');
+		} else
+			if($searchStore !== 0)
+			{
+				//print_r($searchStore);
+				$result = new TextMessageBuilder("Result of " . $search_query . ":\nSort by: " . $sort . "\nPrice: " . $price . "\nSafe Search: " . $safe_search . "\n\nResult:\n\n" . $searchStore);
+			}
 
 		}
 
