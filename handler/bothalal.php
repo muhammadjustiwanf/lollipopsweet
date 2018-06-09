c50dddd4b691885b9d91e8fe5249c23f6dd97b33
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-09 08:55:11 +0700

    Add files via upload

--- handler/bothalal.php
+++ handler/bothalal.php
@@ -22,11 +22,20 @@ function produk($query, $userId){
 
 		$response = Unirest\Request::get("$URL");
 		$json = json_decode($response->raw_body, true);
+		$json = array();
+		$jsonFound = array_push($json,
+				$json['data']['title'],
+				$json['data']['nomor_sertifikat'],
+				$json['data']['produsen'],
+				$json['data']['berlaku_hingga']
+		));
 		
-		if (isset($json['error']))
+		if ($jsonFound = 'error'){
 			$result = new TextMessageBuilder('Produk ' . $query . ' tidak ditemukan. Ngetik yang bener yak, jangan typo! :v');
-		else
-			$result = new TextMessageBuilder("Hasil pencarian dengan nama produk " . strtoupper(urldecode($query)) . ":\nTanggal: " . date('j F Y') . "\n\n\n1.  Nama Produk: " . $json['data']['0']['title'] . "\n     Nomor Sertifikat: " . $json['data']['0']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['0']['produsen'] . "\n     Berlaku hingga: " . $json['data']['0']['berlaku_hingga'] . "\n\n\n2.  Nama Produk: " . $json['data']['1']['title'] . "\n     Nomor Sertifikat: " . $json['data']['1']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['1']['produsen'] . "\n     Berlaku hingga: " . $json['data']['1']['berlaku_hingga'] . "\n\n\n3.  Nama Produk: " . $json['data']['2']['title'] . "\n     Nomor Sertifikat: " . $json['data']['2']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['2']['produsen'] . "\n     Berlaku hingga: " . $json['data']['2']['berlaku_hingga'] . "\n\n\n4.  Nama Produk: " . $json['data']['3']['title'] . "\n     Nomor Sertifikat: " . $json['data']['3']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['3']['produsen'] . "\n     Berlaku hingga: " . $json['data']['3']['berlaku_hingga'] . "\n\n\n5.  Nama Produk: " . $json['data']['4']['title'] . "\n     Nomor Sertifikat: " . $json['data']['4']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['4']['produsen'] . "\n     Berlaku hingga: " . $json['data']['4']['berlaku_hingga'] . "\n\n\n6.  Nama Produk: " . $json['data']['5']['title'] . "\n     Nomor Sertifikat: " . $json['data']['5']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['5']['produsen'] . "\n     Berlaku hingga: " . $json['data']['5']['berlaku_hingga'] . "\n\n\n7.  Nama Produk: " . $json['data']['6']['title'] . "\n     Nomor Sertifikat: " . $json['data']['6']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['6']['produsen'] . "\n     Berlaku hingga: " . $json['data']['6']['berlaku_hingga'] . "\n\n\n8.  Nama Produk: " . $json['data']['7']['title'] . "\n     Nomor Sertifikat: " . $json['data']['7']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['7']['produsen'] . "\n     Berlaku hingga: " . $json['data']['7']['berlaku_hingga'] . "\n\n\n9.  Nama Produk: " . $json['data']['8']['title'] . "\n     Nomor Sertifikat: " . $json['data']['8']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['8']['produsen'] . "\n     Berlaku hingga: " . $json['data']['8']['berlaku_hingga'] . "\n\n\n10. Nama Produk: " . $json['data']['9']['title'] . "\n     Nomor Sertifikat: " . $json['data']['9']['nomor_sertifikat'] . "\n     Produsen: " . $json['data']['9']['produsen'] . "\n     Berlaku hingga: " . $json['data']['9']['berlaku_hingga'] . "\n\n\nDiakses pada pukul: " . date('H:i:s'));
+		} else {
+			$result = new TextMessageBuilder("Hasil pencarian dengan nama produk " . strtoupper(urldecode($query)) . ":\nTanggal: " . date('j F Y') . "\n\n\n" . $jsonFound("\n\n\n") . "\n\n\nDiakses pada pukul: " . date('H:i:s'));
+				
+				}
 		
 		}
 	
