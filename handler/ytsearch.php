fe60f08ac89192c8b5240ab9cd9b20c2e18456de
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-10 18:53:27 +0700

    Add files via upload

---
+++ handler/ytsearch.php
@@ -0,0 +1,26 @@
+<?php
+
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
+
+function ytsearch($query, $userId){
+	
+	$URL = 'https://www.youtube.com/results';
+	
+	if ($query == null){
+		$result = new TextMessageBuilder("YouTube Search Engine.\n\nKetik: .ytsearch [video]\nContoh: .ytsearch vitas\n\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
+	} else {
+
+		$query = urlencode($query);
+		$URL = $URL . '?search_query=' . $query;
+		
+		if ($URL == null){
+			$result = new TextMessageBuilder('Video ' . $query . ' tidak ditemukan.');
+		} else {
+			$result = new TextMessageBuilder('Klik: ' . $URL);
+		}
+		
+	}
+	
+	return $result;
+
+}
