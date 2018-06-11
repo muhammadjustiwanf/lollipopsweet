8ef7c08e6ac2bba1e25f5ff77f2cddd55eddc303
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-12 04:53:53 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ include 'unirest-php-master/src/Unirest.php';
 
 use LINE\LINEBot\SignatureValidator as SignatureValidator;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
-foreach (glob("handler/*.php") as $handler){
+/*foreach (glob("handler/*.php") as $handler){
 		if ($handler != 'handler/post.php'){
 				include $handler;
 		}
-}
+}*/
 
 $dotenv = new Dotenv\Dotenv('env');
 $dotenv->load();
