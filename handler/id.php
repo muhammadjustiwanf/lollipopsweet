4196abbc455a489c3371adbc4a78cc2563a73286
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-11 14:45:30 +0700

    Add files via upload

--- handler/id.php
+++ handler/id.php
@@ -7,10 +7,10 @@ function userid($query, $userId){
 	if ($userId == null){
 		$result = new TextMessageBuilder('Kita belum berteman, add dulu gih sono lalu ketik .userid dan kirim di kolom chat :v');
 	} else {
-		$result = new TextMessageBuilder("Ini adalah userid " . $profil->displayName . ":\n" . $userId . "\n\nDisimpan baik2 yah userid nya");
+		$result = new TextMessageBuilder("Ini adalah userid " . $getProfile->displayName . ":\n" . $userId . "\n\nDisimpan baik2 yah userid nya");
 	}
 	
-	$client->replyMessage($result);
+	return $result;
 	
 }
 
