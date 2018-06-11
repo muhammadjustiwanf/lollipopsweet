b9819601800d3beb6a51300c9b5bf5d83402af46
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-11 18:27:18 +0700

    Add files via upload

--- handler/ytsearch.php
+++ handler/ytsearch.php
@@ -4,23 +4,55 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function ytsearch($query, $userId){
 	
-	$URL = 'https://www.youtube.com/results';
+	include 'line_class.php';
+	include 'unirest-php-master/src/Unirest.php';
+	date_default_timezone_set('Asia/Jakarta');
+	$query = explode(" ", $message['text']);
+	$command = $query[0];
+	$options = $query[1];
+	if (count($query) > 2) {
+    for ($i = 2; $i < count($query); $i++) {
+        $options .= '+';
+        $options .= $query[$i];
+    }
+}
+	$URL = 'http://rahandiapi.herokuapp.com/youtubeapi/search';
 	
 	if ($query == null){
 		$result = new TextMessageBuilder("YouTube Search Engine.\n\nKetik: .ytsearch [video]\nContoh: .ytsearch vitas\n\nSilahkan dicoba~ (((o(*ﾟ▽ﾟ*)o)))");
 	} else {
 
 		$query = urlencode($query);
-		$URL = $URL . '?search_query=' . $query;
+		$URL = $URL . '?key=betakey&q=' . $query;
+		$response = Unirest\Request::get("$URL");
+		$json = json_decode($response->raw_body, true);
 		
-		if ($URL == null){
+		if ($json['error']){
 			$result = new TextMessageBuilder('Video ' . $query . ' tidak ditemukan.');
 		} else {
-			$result = new TextMessageBuilder('Klik: ' . $URL);
-		}
+			$result = new TextMessageBuilder(""Result from " . $query . ":\n\nChannel: " . $json['result']['author'] . "\nJudul: " . $json['result']['title'] . "\nDurasi: " . $json['result']['duration'] . "\nLikes: " . $json['result']['likes'] . "\nDislikes: " . $json['result']['dislikes'] . "\nViewer: " . $json['result']['viewcount'] . "\nLink Thumbnail: " . $json['result']['thumbnail'] . "\n\nDiakses pada pukul: " . date('H:i:s'));
+			}
 		
-	}
-	
+		}
+
 	return $result;
 
 }
+
+			$balas = array(
+            'replyToken' => $replyToken,
+            'messages' => array(
+		            $hasil = ytsearch($options);
+		            $hasill = thumbnail($options);
+                array(
+                    'type' => 'text',
+                    'text'  => $hasil
+                ), array(
+                    'type' => 'image',
+                    'originalContentUrl' => $hasill,
+                    'previewImageUrl' => $hasill
+                )
+            )
+        );
+
+  $client->replyMessage($balas);
