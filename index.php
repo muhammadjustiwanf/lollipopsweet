d93eef9916e69ca13576521c6e7a3aa331633543
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-11 09:51:25 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $app->post('/', function ($request, $response)
 		return $response->withStatus(400, 'Invalid signature');
 	}
 	
-	//$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
+	$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 
@@ -53,14 +53,14 @@ $app->post('/', function ($request, $response)
 				// --------------------------------------------------------------- NOTICE ME...
 				
 				$inputMessage = $event['message']['text'];
-				/*$userId = $client->parseEvents()[0]['source']['userId'];
+				$userId = $client->parseEvents()[0]['source']['userId'];
 				$replyToken = $client->parseEvents()[0]['replyToken'];
 				$timestamp	= $client->parseEvents()[0]['timestamp'];
 				$type = $client->parseEvents()[0]['type'];
 				$message 	= $client->parseEvents()[0]['message'];
 				$messageid = $client->parseEvents()[0]['message']['id'];
 				$profil = $client->profil($userId);
-*/
+
 				if ($inputMessage[0] == '.') {
 
 					 $inputMessage = ltrim($inputMessage, '.');
