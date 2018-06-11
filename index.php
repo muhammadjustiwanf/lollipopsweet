ae9952c12ae97169141ca6bdd5d354e1a5cf829f
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-11 19:29:12 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,52 +1,35 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-//require_once('./line_class.php');
-//require_once('./unirest-php-master/src/Unirest.php');
+require 'vendor/autoload.php';
 
-use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+use LINE\LINEBot\SignatureValidator as SignatureValidator;
 
-//var_dump($client->parseEvents());
-
-
-//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
-
-// load config
-$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv = new Dotenv\Dotenv('env');
 $dotenv->load();
 
-// initiate app
 $configs =  [
 	'settings' => ['displayErrorDetails' => true],
 ];
 $app = new Slim\App($configs);
 
-/* ROUTES */
 $app->get('/', function ($request, $response) {
-	return "Lanjutkan!";
+	return "Sukses mendeploy. Silahkan dicoba botnya";
 });
 
 $app->post('/', function ($request, $response)
 {
-	// get request body and line signature header
 	$body 	   = file_get_contents('php://input');
 	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
-
-	// log body and signature
 	file_put_contents('php://stderr', 'Body: '.$body);
-
-	// is LINE_SIGNATURE exists in request header?
+	
 	if (empty($signature)){
 		return $response->withStatus(400, 'Signature not set');
 	}
-
-	// is this request comes from LINE?
+	
 	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
 		return $response->withStatus(400, 'Invalid signature');
 	}
-
-	// init bot
-	$client = new LINEBotTiny($_ENV['CHANNEL_ACCESS_TOKEN'], $_ENV['CHANNEL_SECRET']);
+	
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 
