05d514f87b2cac5999a32ecd0127b2a7d72603ea
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-11 20:26:25 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -73,6 +73,50 @@ $app->post('/', function ($request, $response)
 
 }
 
+			if($event['message']['type'] == 'text')
+			{
+				
+				// --------------------------------------------------------------- NOTICE ME...
+				
+				$inputMessage = $event['message']['text'];
+				$userId = $event['source']['userId'];
+
+				if ($inputMessage[0] == '.') {
+
+					 $inputMessage = ltrim($inputMessage, '.');
+					 $inputSplit = explode(' ', $inputMessage, 2);
+
+					 if ( function_exists( $inputSplit[0] ) ){
+
+							$outputMessage = $inputSplit[0]( $inputSplit[1], $userId );
+
+					 } else {
+				$outputMessage = new TextMessageBuilder('tipe command tidak ditemukan :v');
+					 }
+				
+				$result = $bot->replyMessage($event['replyToken'], $outputMessage);
+				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+
+} else {
+
+				$wordsLearned = file_get_contents('https://bot-line-multifunction.firebaseio.com/words.json');
+				$wordsLearned = json_decode($wordsLearned, true);
+
+				foreach ($wordsLearned as $word => $answer) {
+						if (strpos(strtolower($inputMessage), $word) !== false) {
+								$outputMessage = new TextMessageBuilder($answer);
+								$result = $bot->replyMessage($event['replyToken'], $outputMessage);
+								return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+								break;
+						}
+				}
+
+}
+				
+				// --------------------------------------------------------------- ...SENPAI!
+				
+			}
+
 $pesan_datang = explode(" ", $message['text']);
 $msg_type = $message['type'];
 $command = $pesan_datang[0];
@@ -1603,50 +1647,6 @@ if (isset($balas)) {
 
     $client->replyMessage($balas);
 }
-
-			if($event['message']['type'] == 'text')
-			{
-				
-				// --------------------------------------------------------------- NOTICE ME...
-				
-				$inputMessage = $event['message']['text'];
-				$userId = $event['source']['userId'];
-
-				if ($inputMessage[0] == '.') {
-
-					 $inputMessage = ltrim($inputMessage, '.');
-					 $inputSplit = explode(' ', $inputMessage, 2);
-
-					 if ( function_exists( $inputSplit[0] ) ){
-
-							$outputMessage = $inputSplit[0]( $inputSplit[1], $userId );
-
-					 } else {
-				$outputMessage = new TextMessageBuilder('tipe command tidak ditemukan :v');
-					 }
-				
-				$result = $bot->replyMessage($event['replyToken'], $outputMessage);
-				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-
-} else {
-
-				$wordsLearned = file_get_contents('https://bot-line-multifunction.firebaseio.com/words.json');
-				$wordsLearned = json_decode($wordsLearned, true);
-
-				foreach ($wordsLearned as $word => $answer) {
-						if (strpos(strtolower($inputMessage), $word) !== false) {
-								$outputMessage = new TextMessageBuilder($answer);
-								$result = $bot->replyMessage($event['replyToken'], $outputMessage);
-								return $result->getHTTPStatus() . ' ' . $result->getRawBody();
-								break;
-						}
-				}
-
-}
-				
-				// --------------------------------------------------------------- ...SENPAI!
-				
-			}
 		}
 	}
 
