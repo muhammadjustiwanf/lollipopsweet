4a6d40a0016714fbde309c3b4570bcf2cd1fa7f1
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-12 10:24:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -103,21 +103,21 @@ if ($message['type'] == 'text'){
 										          0 => 
 										          array (
 										            'type' => 'postback',
-										            'label' => 'Cari Anime',
+										            'label' => 'Instagram Random Photo',
 										            'data' => 'action=add&itemid=111',
 													'text' => '.instagram'
 										          ),
 										          1 => 
 										          array (
 										            'type' => 'postback',
-										            'label' => 'Cari Sinopsis Anime',
+										            'label' => 'Kalkulator',
 										            'data' => 'action=add&itemid=111',
 													'text' => '.calculate'
 												  ),
 										          2 => 
 										          array (
 										            'type' => 'postback',
-										            'label' => 'Cari Manga',
+										            'label' => 'User ID',
 										            'data' => 'action=add&itemid=111',
 													'text' => '.userid'
 										          ),
