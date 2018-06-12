0dfd33b2cc1d36e302a1601e0586021d8c6b69d0
Muhammad Justiwan Farnadi <30584566+muhammadjustiwanf@users.noreply.github.com>
2018-06-12 10:42:08 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ if ($message['type'] == 'text'){
 										          0 => 
 										          array (
 										            'type' => 'postback',
-										            'label' => 'Instagram Random Phot',
+										            'label' => 'Instagram Random P',
 										            'data' => 'action=add&itemid=111',
 													'text' => '.instagram'
 										          ),
